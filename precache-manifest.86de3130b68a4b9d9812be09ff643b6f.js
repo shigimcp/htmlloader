ba32b70d929cfaf1ed3179a347807633c97f70f3
shigimcp <shigimcp@hotmail.com>
2020-07-09 02:37:40 -0400

    Updates

---
+++ precache-manifest.86de3130b68a4b9d9812be09ff643b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0b8d2de632e1ef75535ad6cde1dbaf",
+    "url": "/htmlloader/index.html"
+  },
+  {
+    "revision": "4708ea77f6e9bff9811b",
+    "url": "/htmlloader/static/css/main.5b721eee.chunk.css"
+  },
+  {
+    "revision": "0dd95bbff52a5652dfe7",
+    "url": "/htmlloader/static/js/2.a0584b61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/htmlloader/static/js/2.a0584b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4708ea77f6e9bff9811b",
+    "url": "/htmlloader/static/js/main.3274ecbf.chunk.js"
+  },
+  {
+    "revision": "b7fa4a12f4fe6170cc83129edd5c527f",
+    "url": "/htmlloader/static/js/main.3274ecbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d19dc773f3a242670a9",
+    "url": "/htmlloader/static/js/runtime-main.5b4d46fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/htmlloader/precache-manifest.9912ecd9e7221c54707cf21e0fd5ea80.js"
+  "/htmlloader/precache-manifest.86de3130b68a4b9d9812be09ff643b6f.js"
 );
 
 self.addEventListener('message', (event) => {
